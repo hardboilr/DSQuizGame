736cbcc0b27b55932565b41f9a729c0608afcab1
Andreas Styltsvig <Andreas Styltsvig@10.50.138.25>
2014-10-30 12:34:41 +0100

    Updated quizGUI2 with quizcontroller

--- src/Andreas/QuizControl.java
+++ src/Andreas/QuizControl.java
@@ -2,6 +2,7 @@
 package Andreas;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  *
@@ -13,15 +14,48 @@ public class QuizControl {
     ArrayList<String> answer;
     FileHandler fileHandler = new FileHandler();
     ArrayList<String> file;
+    ArrayList<String> pictureURL;
+    
+    Random random = new Random();
     
     public void getFile(String filename){
         file = new ArrayList();
+        answer = new ArrayList();
+        pictureURL = new ArrayList();
+        
         file = fileHandler.load(filename);
+        question = file.get(0).substring(file.get(0).indexOf(":")+2, file.get(0).indexOf("?")+1);
         
-        question = file.get(0).substring(file.get(0).indexOf(":"), file.get(0).indexOf("?")+1);
+        for (int i = 0; i < 4; i++) { 
+            answer.add(file.get(i+1));
+        }
+        for (int i = 0; i < 4; i++) { 
+            pictureURL.add(file.get(i+5));
+        }
         
-        System.out.println(question);
+        
+        
+        
     }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public ArrayList<String> getAnswer() {
+        return answer;
+    }
+    
+    public String getRandomFile(){
+        String randomFile = "Question"+random.ints(2).toString();
+        return randomFile;
+    }
+    
+    public ArrayList<String> getURLS(){
+        return pictureURL;
+    }
+    
+    
     
     
     
